28b0e3da67aa26d35ede1c3a1a44264f80dd708d
JaimeRC <jaime.rc@icloud.com>
2018-10-23 19:19:43 +0200

    add artist component

--- SpotifyApp/src/app/components/artist/artist.component.ts
+++ SpotifyApp/src/app/components/artist/artist.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-artist',
   templateUrl: './artist.component.html',
   styles: []
 })
-export class ArtistComponent implements OnInit {
+export class ArtistComponent {
 
-  constructor() { }
+  artist: any = {}
+  loadingArtist: boolean
+  topTracks: any[]
 
-  ngOnInit() {
+  constructor(private router: ActivatedRoute, private spotify: SpotifyService) {
+    this.loadingArtist = true
+    this.router.params.subscribe(params => {
+      this.getArtist(params['id'])
+      this.getTopTracks(params['id'])
+    })
   }
 
+  getArtist(id: string) {
+
+    this.spotify.getArtist(id)
+      .subscribe((data: any) => {
+        this.loadingArtist = false
+        this.artist = data
+      })
+  }
+
+  getTopTracks(id: string) {
+    this.spotify.getTopTracks(id)
+      .subscribe((topTracks: any) => {
+        this.topTracks = topTracks
+      })
+  }
 }
