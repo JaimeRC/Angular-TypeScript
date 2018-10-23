dceaa03aac70924f941c3af6be6aeaf63f48c56f
JaimeRC <jaime.rc@icloud.com>
2018-10-23 19:21:05 +0200

    add spotify servie

--- SpotifyApp/src/app/services/spotify.service.ts
+++ SpotifyApp/src/app/services/spotify.service.ts
@@ -11,8 +11,8 @@ export class SpotifyService {
   constructor(private http: HttpClient) { }
 
   getQuery(query: string) {
-    const url:string = `https://api.spotify.com/v1/${query}`
-    const headers = new HttpHeaders({ 'Authorization': 'Bearer BQCXuoSp3AxpB-VfdohqmBqjBJRWY8Y1Ag1JBJAInzTpJM3qclKMZv89E3iq5H1zGWL5_PSMpkVY9dzdFDI' })
+    const url: string = `https://api.spotify.com/v1/${query}`
+    const headers = new HttpHeaders({ 'Authorization': 'Bearer BQD-paV0eqCTn3JFAmFKlugIiLzhv3MnEJ226eC7Zy043zrS8oU7sxnvJX6v-oygIcH_dhriY1r8BWfYKFE' })
 
     return this.http.get(url, { headers })
   }
@@ -22,8 +22,18 @@ export class SpotifyService {
       .pipe(map(data => data['albums'].items))
   }
 
-  getArtirsts(termino: string) {
+  getArtists(termino: string) {
     return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
       .pipe(map(data => data['artists'].items))
+  }
+
+  getArtist(id: string) {
+    return this.getQuery(`artists/${id}`)
+
+  }
+
+  getTopTracks(id: string) {
+    return this.getQuery(`artists/${id}/top-tracks?country=es`)
+      .pipe(map(data => data['tracks']))
   }
 }
