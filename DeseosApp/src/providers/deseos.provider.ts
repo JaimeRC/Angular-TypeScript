2b808bae69069e6dfc9ace2bbf9d8ea4c7d50fd3
JaimeRC <jaime.rc@icloud.com>
2018-10-25 22:06:20 +0200

    changes Providers

--- DeseosApp/src/providers/deseos.provider.ts
+++ DeseosApp/src/providers/deseos.provider.ts
@@ -4,13 +4,31 @@ import { List } from "../models";
 @Injectable()
 export class DeseosProvider {
 
-    list: List[] = []
+    lists: List[] = []
 
     constructor() {
-        const list1 = new List('Recolectar piedras del infinito')
-        const list2 = new List('Heroes a vencer')
+        this.loadStorage()
+    }
 
-        this.list.push(list1)
-        this.list.push(list2)
+    addList(newList: List) {
+        this.lists.push(newList)
+        this.saveStorage()
+    }
+
+    removeList(oldList: List) {
+        this.lists = this.lists.filter(listData => listData.id !== oldList.id)
+        this.saveStorage()
+    }
+
+    loadStorage() {
+        if (localStorage.getItem('data')) {
+            this.lists = JSON.parse(localStorage.getItem('data'))
+        } else {
+            this.lists = []
+        }
+    }
+
+    saveStorage() {
+        localStorage.setItem('data', JSON.stringify(this.lists))
     }
 }
