9bd8f0d25f45bb3bc56187b592f64f10fa3e855a
JaimeRC <jaime.rc@icloud.com>
2018-10-25 22:05:01 +0200

    changes Pendientes Page

--- DeseosApp/src/pages/pendientes/pendientes.component.ts
+++ DeseosApp/src/pages/pendientes/pendientes.component.ts
@@ -1,13 +1,38 @@
 import { Component } from "@angular/core";
 import { DeseosProvider } from "../../providers/deseos.provider";
+import { NavController, AlertController } from "ionic-angular";
+import { AgregarPage } from "../agregar/agregar.component";
 
 @Component({
     selector: 'page-pendientes',
     templateUrl: 'pendientes.component.html'
 })
-export class PendientePage {
+export class PendientesPage {
 
-    constructor(public deseosService: DeseosProvider) {
+    constructor(public deseosService: DeseosProvider,
+        private navCtrl: NavController,
+        public alertCtrl: AlertController) {
+    }
 
+    addlist() {
+        const alert = this.alertCtrl.create({
+            title: 'Nueva Lista',
+            message: 'Nombre de la nueva lista de Deseos',
+            inputs: [{ name: 'Titulo', placeholder: 'Nombre de la Lista' }],
+            buttons: [{
+                text: 'Cancelar'
+            }, {
+                text: 'Agregar',
+                handler: data => {
+                    if (data.title.length === 0) {
+                        return
+                    }
+                    this.navCtrl.push(AgregarPage, {
+                        title: data.title
+                    })
+                }
+            }]
+        })
+        alert.present()
     }
 }
